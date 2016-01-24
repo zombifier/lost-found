7921ea2600cbfc1f3242c3576f1f64ed122ed70f
Khoi Doan <zombify.25@gmail.com>
2016-01-23 23:46:24 -0500

    added global navbar

--- client/lost-found.js
+++ client/lost-found.js
@@ -5,7 +5,19 @@ Router.route('/', function () {
 Router.route('/lost');
 Router.route('/found');
 
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
 if (Meteor.isClient) {
+  Template.navItems.helpers({
+    isActive: function (template) {
+      var currentRoute = Router.current();
+      return currentRoute &&
+        template === currentRoute.lookupTemplate() ? 'active' : '';
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
