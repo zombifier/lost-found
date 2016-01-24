d03e7a33a0ec3dfd0f7b51337764df4b0423ad6b
Khoi Doan <zombify.25@gmail.com>
2016-01-24 02:05:52 -0500

    new template for found.html

--- client/lost-found.js
+++ client/lost-found.js
@@ -9,6 +9,8 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
+Found = new Mongo.Collection("found");
+
 if (Meteor.isClient) {
   Template.navItems.helpers({
     isActive: function (template) {
@@ -17,5 +19,32 @@ if (Meteor.isClient) {
         template === currentRoute.lookupTemplate() ? 'active' : '';
     }
   });
+  
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+  
+  Template.found.events({
+    "submit .info": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      //var text = event.target.text.value;
 
+      console.log(event.description);
+      // Insert a task into the collection
+      /*Tasks.insert({
+        text: text,
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(), // _id of logged in user
+        username: Meteor.user().username
+      });
+ 
+      // Clear form
+      event.target.text.value = "";*/
+    },
+  });
 }
+
+
