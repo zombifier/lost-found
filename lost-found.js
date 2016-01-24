99e14b708dacf7e6ad21f8b68330f7a6925603ce
Khoi Doan <zombify.25@gmail.com>
2016-01-23 22:56:59 -0500

    added iron-router

--- lost-found.js
+++ lost-found.js
@@ -1,3 +1,13 @@
+Router.route('/', function () {
+    this.render('home');
+});
+
+
+
+Router.route('/lost');
+
+Router.route('/found');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -21,3 +31,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
